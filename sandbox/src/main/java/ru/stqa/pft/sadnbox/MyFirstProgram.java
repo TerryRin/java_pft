a6a5c55fe7ebce8dcbccab024e3efe654346450c
TerryRin <terryrinkelly@gmail.com>
2021-05-14 23:47:52 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sadnbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sadnbox/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.sadnbox;
 
 public class MyFirstProgram {
-	
+
   public static void main(String[] args) {
-      System.out.println("Hello, world!");
-	}
+    System.out.println("Hello, world!");
+  }
 
 }
